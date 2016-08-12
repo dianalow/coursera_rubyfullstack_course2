56b3eb18b2f7ee9ef3c669d7ed86feb362139d34
Diana Low <lowdiana@gmail.com>
2016-08-12 17:13:34 +0800

    final

---
+++ week4/todolists/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  skip_before_action :ensure_login, only: [:new, :create]
+  def new
+  end
+
+  def create
+  	username = User.find_by(username: params[:user][:username])
+  	password = params[:user][:password]
+
+  	if username && username.authenticate(password)
+  		session[:username_id] = username.id
+  		redirect_to root_path, notice: "Logged in successfully."
+  	else
+  		redirect_to login_path, alert: "Invalid login."
+  	end
+  end
+
+  def destroy
+  	reset_session
+  	redirect_to login_path, notice: "Logged out successfully."
+  end
+end
